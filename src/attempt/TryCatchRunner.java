28506d2d488e532fd5daac4755154919838ea40e
Deadeyes13 <jaren.paul@gmail.com>
2014-01-23 09:51:24 -0700

    Adding to repo

---
+++ src/attempt/TryCatchRunner.java
@@ -0,0 +1,46 @@
+package attempt;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author CJ Oman
+ * @version 1.1
+ * @date Dec 10, 2013 9:04:00 AM
+ */
+public class TryCatchRunner
+{
+	public static void main(String[] args)
+	{
+		
+//		int testNumber = 15;
+//		double testFloatingPoint = 0.0000001;
+//		double errorTest = 0.0;
+//		
+//		int otherNumber = 0;
+//		
+//		try
+//		{
+//			testNumber = testNumber / otherNumber;
+//		}
+//		catch(ArithmeticException currentError)
+//		{
+//			JOptionPane.showMessageDialog(null, currentError.getMessage());
+//			JOptionPane.showMessageDialog(null, currentError.getClass().toString());
+//		}
+//
+//		try
+//		{
+//			errorTest = errorTest / errorTest;
+//		}
+//		catch(ArithmeticException currentError)
+//		{
+//			JOptionPane.showMessageDialog(null, currentError.getMessage());
+//			JOptionPane.showMessageDialog(null, currentError.getClass().toString());
+//		}
+//		JOptionPane.showMessageDialog(null,"Funny answer: " + 1/errorTest );
+
+		Object x = new Integer(0);
+	     System.out.println("");
+	}
+
+}
